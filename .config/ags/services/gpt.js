31b17ee3d5c289ca080fcc982483e1bf5b157b88
Celes Renata <celes@celestium.life>
2024-07-17 21:01:18 -0700

    typo

--- .config/ags/services/gpt.js
+++ .config/ags/services/gpt.js
@@ -45,7 +45,7 @@ const initMessages =
 // We're using many models to not be restricted to 3 messages per minute.
 // The whole chat will be sent every request anyway.
 Utils.exec(`mkdir -p ${GLib.get_user_cache_dir()}/ags/user/ai`);
-const CHAT_MODELS = [ "llama3:instruct" "codellama:7b-instruct" ]
+const CHAT_MODELS = [ "llama3:instruct", "codellama:7b-instruct" ]
 
 class GPTMessage extends Service {
     static {
