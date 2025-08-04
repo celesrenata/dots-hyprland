42b7a897500a4e0082bc2c4fd0f9157b6ab72404
Celes Renata <celes@frameshift.net>
2025-08-03 21:35:27 -0700

    Add missing system tools to quickshell service PATH
    - Added bash, procps, util-linux for system commands (pidof, bash -c)
    - Added translate-shell for translation features
    - Added imagemagick for image processing (magick identify)
    - Added hyprland for hyprctl commands
    - Services are now attempting system calls but missing these tools

--- modules/components/quickshell.nix
+++ modules/components/quickshell.nix
@@ -117,7 +117,13 @@ in
           "QT_SCALE_FACTOR=${toString cfg.scaling}"
           "QT_QUICK_CONTROLS_STYLE=Basic"
           "QML2_IMPORT_PATH=${config.xdg.configHome}/quickshell/ii:${pkgs.qt6.qtdeclarative}/lib/qt-6/qml:${pkgs.qt6.qt5compat}/lib/qt-6/qml"
-          "PATH=${lib.makeBinPath (with pkgs; [ coreutils findutils gnused gnugrep gawk curl wget jq playerctl htop ])}"
+          "PATH=${lib.makeBinPath (with pkgs; [ 
+            coreutils findutils gnused gnugrep gawk curl wget jq playerctl htop
+            bash procps util-linux # pidof, bash commands
+            translate-shell # trans command
+            imagemagick # magick command for image processing
+            hyprland # hyprctl command
+          ])}"
           "XDG_CONFIG_HOME=${config.xdg.configHome}"
           "XDG_DATA_HOME=${config.xdg.dataHome}"
           "XDG_CACHE_HOME=${config.xdg.cacheHome}"
